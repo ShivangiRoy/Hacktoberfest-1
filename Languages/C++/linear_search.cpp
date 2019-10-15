0a3ce00a6020d8529839ba1d7bceb98ccb28732a
Shivangi Roy <shivangi.sr22@gmail.com>
2019-10-16 01:57:18 +0530

    first commit

--- Languages/C++/linear_search.cpp
+++ Languages/C++/linear_search.cpp
@@ -1,7 +1,3 @@
-//Linear search in given array .
-//return index of searching element if it is present .
-
-
 #include <iostream>
 using namespace std ;
 
@@ -10,25 +6,19 @@ int main()
 	int n;
 	cout<<"Number of Elements"<<endl;
 	cin>>n;
-	int A[n+1];int index_key = -1;
+	int A[n], index = -1, x;
+	cout<<"Enter search element: ";
+	cin>>x;
+
 	for(int i=0;i<n;i++)
 	{
 		cin>>A[i];
-
+		if(A[i]==x)
+			index=i;
 	}
-	cout<<"searching Element"<<endl;
-	int search_Key;
-	cin>>search_Key;
-	for(int i=0;i<n;i++)
+	if(index != -1)
 	{
-		if(A[i] == search_Key)
-		{
-			index_key = i;
-		}
-	}
-	if(index_key != -1)
-	{
-		cout<<"Element is present at index "<<index_key<<endl;
+		cout<<"Element is present at index "<<index<<endl;
 	}
 	else
 		cout<<"Element is not present in given array ."<<endl ;
